2cca4bb69f7c8a6a6aafbfe322edc9f7c156bde8
Tonywhite11 <plumfish464@gmail.com>
2025-05-29 01:59:38 -0700

    restarted backend

--- Backend.py
+++ Backend.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://work-flows2-tony.vercel.app/"], # Adjust for production
+    allow_origins=["https://work-flows2-tony.vercel.app"], # Adjust for production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
